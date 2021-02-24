310170846bd88b51ef58d7de2213183f1202e247
JedlikSV <susleczviki26@gmail.com>
2021-02-24 12:38:18 +0100

    first commit

--- StarWars/StarWars.py
+++ StarWars/StarWars.py
@@ -5,9 +5,13 @@ import time
 space = turtle.Screen()
 space.setup(width=800, height=600)
 space.bgpic("space_.png")
-space.addshape
+space.addshape("sprite.gif")
 
 urhajo = turtle.Turtle()
+urhajo.shape("sprite.gif")
 
 while True:
-    
+
+    space.update()
+    time.sleep(0.2)
+
