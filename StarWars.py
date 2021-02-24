7b14ba5c22413bc1e0b7d4e9f260277bd677da7d
JedlikSV <susleczviki26@gmail.com>
2021-02-24 12:25:59 +0100

    ez egy komoly játék

---
+++ StarWars.py
@@ -0,0 +1,13 @@
+import turtle
+import random
+import time
+
+space = turtle.Screen()
+space.setup(width=800, height=600)
+space.bgpic("space_.png")
+space.addshape
+
+urhajo = turtle.Turtle()
+
+while True:
+    
